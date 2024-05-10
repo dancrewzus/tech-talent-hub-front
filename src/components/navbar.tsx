a9e099c07de0dc2a892e6401d41423ad210be4d6
Alvaro Resplandor <alvarojrr79@gmail.com>
2024-05-10 00:09:31 -0400

    feat: initialize user menu

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,17 @@
+import { LifeBuoy, LogOut, User } from "lucide-react";
 import { Link } from "react-router-dom";
 
 import { Drawer, DrawerItem } from "./drawer";
 import { Button } from "./ui/button";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 export function NavBar() {
 	return (
@@ -11,22 +21,50 @@ export function NavBar() {
 					<p className="font-medium">Tech Talent Hub</p>
 				</a>
 
-				<div className="block md:hidden">
-					<Drawer>
-						<DrawerItem title="Iniciar Sesión" to="/login" />
+				<div className="flex gap-2">
+					<DropdownMenu>
+						<DropdownMenuTrigger asChild>
+							<Button variant="ghost" size="icon">
+								<User className="h-4 w-4" />
+							</Button>
+						</DropdownMenuTrigger>
 
-						<DrawerItem title="Registrarse" to="/sign-up" />
-					</Drawer>
-				</div>
+						<DropdownMenuContent className="w-56">
+							<DropdownMenuLabel>John Doe</DropdownMenuLabel>
 
-				<div className="hidden space-x-2 md:block">
-					<Button variant="ghost" asChild>
-						<Link to="/login">Iniciar Sesión</Link>
-					</Button>
+							<DropdownMenuSeparator />
 
-					<Button asChild>
-						<Link to="/sign-up">Registrarse</Link>
-					</Button>
+							<DropdownMenuGroup>
+								<DropdownMenuItem>
+									<LifeBuoy className="mr-2 h-4 w-4" />
+									<span>Soporte</span>
+								</DropdownMenuItem>
+
+								<DropdownMenuItem>
+									<LogOut className="mr-2 h-4 w-4" />
+									<span>Cerrar sesión</span>
+								</DropdownMenuItem>
+							</DropdownMenuGroup>
+						</DropdownMenuContent>
+					</DropdownMenu>
+
+					<div className="block md:hidden">
+						<Drawer>
+							<DrawerItem title="Iniciar Sesión" to="/login" />
+
+							<DrawerItem title="Registrarse" to="/sign-up" />
+						</Drawer>
+					</div>
+
+					<div className="hidden space-x-2 md:block">
+						<Button variant="ghost" asChild>
+							<Link to="/login">Iniciar Sesión</Link>
+						</Button>
+
+						<Button asChild>
+							<Link to="/sign-up">Registrarse</Link>
+						</Button>
+					</div>
 				</div>
 			</div>
 		</nav>
