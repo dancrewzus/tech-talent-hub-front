04aba7608d7d147f0e279e95ce686555e9e4178a
helvis <helvis.eloy1998@gmail.com>
2024-06-13 16:36:44 -0400

    avanzando 2

---
+++ src/features/offers/pages/components/sidebar.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export function Sidebar() {
+	return (
+		<div className="items-left flex flex-col">
+			<p className="text-lg font-bold">Create Offer</p>
+
+			<Button className="my-2 w-20 bg-white text-black">General</Button>
+			<Button className="my-2 w-20 bg-white text-black">Areas</Button>
+		</div>
+	);
+}
--- src/features/offers/pages/offers-create.tsx
+++ src/features/offers/pages/offers-create.tsx
@@ -1,9 +1,11 @@
 import { Main } from "@/components/main";
 
+import { Sidebar } from "./components/sidebar";
+
 export function OffersCreate() {
 	return (
 		<Main className="mx-auto w-full max-w-screen-xl space-y-8 pb-12 sm:px-6 sm:pt-12">
-			<h2>hola kidwolf</h2>
+			<Sidebar></Sidebar>
 		</Main>
 	);
 }
