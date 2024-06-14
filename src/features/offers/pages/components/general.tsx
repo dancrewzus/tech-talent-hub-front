ec06787324fa3fbac5808eb464b6690d69208e3b
helvis <helvis.eloy1998@gmail.com>
2024-06-13 20:02:13 -0400

    esperando por el select

---
+++ src/features/offers/pages/components/general.tsx
@@ -0,0 +1,28 @@
+import { Main } from "@/components/main";
+//import { Label } from "@/components/ui/label";
+//import { Input } from "@/components/ui/input";
+//import { Button } from "@/components/ui/button"
+import { DropdownMenu } from "@/components/ui/dropdown-menu";
+
+export function General() {
+	return (
+		<Main className="flex w-full max-w-sm items-center space-x-2">
+			{/* Area */}
+			<DropdownMenu></DropdownMenu>
+
+			{/* Job */}
+
+			{/* Level */}
+
+			{/* Years of experience */}
+
+			{/* Type */}
+
+			{/* Tags */}
+
+			{/* Salary */}
+
+			{/* Hiring Date */}
+		</Main>
+	);
+}
