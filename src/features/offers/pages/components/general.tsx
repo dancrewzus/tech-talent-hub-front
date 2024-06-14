f3c778f6c6e1bdd05ab2dfec4e815097b5ee5b83
helvis <helvis.eloy1998@gmail.com>
2024-06-13 20:38:53 -0400

    componente select listo

--- src/features/offers/pages/components/general.tsx
+++ src/features/offers/pages/components/general.tsx
@@ -2,13 +2,27 @@ import { Main } from "@/components/main";
 //import { Label } from "@/components/ui/label";
 //import { Input } from "@/components/ui/input";
 //import { Button } from "@/components/ui/button"
-import { DropdownMenu } from "@/components/ui/dropdown-menu";
+import { SelectField } from "@/components/select-field";
 
 export function General() {
 	return (
 		<Main className="flex w-full max-w-sm items-center space-x-2">
 			{/* Area */}
-			<DropdownMenu></DropdownMenu>
+			<SelectField
+				id="area"
+				labelProps={{
+					children: "Área",
+				}}
+				options={[
+					{
+						label: "Opcion 1",
+						value: "1",
+					},
+				]}
+				selectProps={{
+					defaultValue: "1",
+				}}
+			/>
 
 			{/* Job */}
 
--- src/features/offers/pages/offers-create.tsx
+++ src/features/offers/pages/offers-create.tsx
@@ -1,11 +1,11 @@
 import { Main } from "@/components/main";
 
-import { Sidebar } from "./components/sidebar";
+import { General } from "./components/general";
 
 export function OffersCreate() {
 	return (
 		<Main className="mx-auto w-full max-w-screen-xl space-y-8 pb-12 sm:px-6 sm:pt-12">
-			<Sidebar></Sidebar>
+			<General></General>
 		</Main>
 	);
 }
