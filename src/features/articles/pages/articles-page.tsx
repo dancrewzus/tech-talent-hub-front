6cbc24ec537031f232358dc0d98a8b90a53ce848
Jose Emilio <j.e.ydarkserver@gmail.com>
2024-06-15 10:43:13 -0400

    Avanzado la vista.
    Signed-off-by: Jose Emilio <j.e.ydarkserver@gmail.com>

--- src/features/articles/pages/articles-page.tsx
+++ src/features/articles/pages/articles-page.tsx
@@ -1,5 +1,8 @@
 import { Main } from "@/components/main";
 
+import { General } from "./components/general";
+
 export function ArticlesPage() {
 	return <Main />;
+	<General></General>;
 }
---
+++ src/features/articles/pages/components/general.tsx
@@ -0,0 +1,55 @@
+import { Main } from "@/components/main";
+import { SelectField } from "@/components/select-field";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export function General() {
+	return (
+		<Main className="flex w-full max-w-sm flex-col space-y-4">
+			{/* Area */}
+			<div className="flex items-center space-x-2">
+				<div className="flex flex-col space-y-2">
+					<Label>Area</Label>
+					<SelectField
+						id="area"
+						labelProps={{
+							children: "",
+						}}
+						options={[
+							{
+								label: "Opcion 1",
+								value: "1",
+							},
+						]}
+						selectProps={{
+							defaultValue: "1",
+						}}
+					/>
+				</div>
+				<div className="mt-6">
+					<Button type="button">Manage areas</Button>
+				</div>
+			</div>
+
+			{/*Titulo */}
+			<div className="flex flex-col space-y-2">
+				<Label>Titulo</Label>
+				<Input type="text" />
+			</div>
+
+			<div className="flex flex-col space-y-2">
+				<Label>Texto</Label>
+				<Input type="text" />
+			</div>
+
+			{/* Tags */}
+			<div className="flex flex-col space-y-2">
+				<Label>Tags</Label>
+				<Input type="text" />
+			</div>
+
+			<Button type="submit">Save changes</Button>
+		</Main>
+	);
+}
